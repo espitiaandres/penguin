b9fe9f3829e770d6aee1bdbd809f26e321241422
Andres Espitia <espitiaandres123@gmail.com>
2022-10-23 07:51:52 -0400

    remove pycache folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="penguin_py",
-    version="0.2.2",
+    version="0.2.3",
     description="Penguin: a customizable stopwatch decorator",
     author="espitiaandres",
     url="https://github.com/espitiaandres/penguin",
