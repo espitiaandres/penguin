e95bf5729a80b5a7c28cffea1dcc02a4516a05fd
Andres Espitia <espitiaandres123@gmail.com>
2022-10-26 00:07:29 -0400

    version 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README_PYPI.md", "r") as fh:
 
 setup(
     name="penguin_py",
-    version="0.2.10",
+    version="0.3.1",
     description="Penguin: a customizable stopwatch decorator",
     author="espitiaandres",
     url="https://github.com/espitiaandres/penguin",
